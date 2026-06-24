ff14008df37fa0a4bd8038da115018d56790ed38
Nicolas <adilsonq1976@gmail.com>
2026-06-24 14:32:22 -0300

    Criando Classe Main

---
+++ src/minhasmusicas/modelos/Main/Main.java
@@ -0,0 +1,37 @@
+package minhasmusicas.modelos.Main;
+
+import minhasmusicas.modelos.MinhasPreferidas;
+import minhasmusicas.modelos.Musica;
+import minhasmusicas.modelos.Podcast;
+
+public class Main {
+    public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Beat it");
+        minhaMusica.setCantor("Michael Jackson");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("CoffeeDev");
+        meuPodcast.setApresentador("Nicolas Tamashiro");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduz();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+        }
+
+        MinhasPreferidas preferidas = new MinhasPreferidas();
+        preferidas.inclui(meuPodcast);
+        preferidas.inclui(minhaMusica);
+    }
+}
